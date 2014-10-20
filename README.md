9c9c44499e6cfd8ac1b0902a3580be59440ae6f9
Diego Freniche Brito <diego@freniche.com>
2014-10-20 13:18:03 +0200

    Now taking photos work, also canceling. Not saving

---
+++ README.md
@@ -0,0 +1,7 @@
+#SwiftCamera
+
+Simple Swift example on how to use the iPhone Camera
+
+Just shows you a preview of the image. You need to save that to the photo roll
+
+__Important__: you can only test this App using a real device. It will NOT work with the iOS simulator
--- SwiftCamera/ViewController.swift
+++ SwiftCamera/ViewController.swift
@@ -1,12 +1,49 @@
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     
     }
 
+    
+    @IBAction func takePhoto(sender: AnyObject) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self;
+            imagePicker.sourceType = .Camera;
+            imagePicker.mediaTypes = NSArray(object: kUTTypeImage)
+            imagePicker.allowsEditing = false;
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+
+        
+    }
+    
+
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        
+        // close Take Photo View Controller
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        self.imageView.image = image;
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+       println("Cancelled")
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+
+    }
+    
+    
 }
 
