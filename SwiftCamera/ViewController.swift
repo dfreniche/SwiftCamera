ea727f378c11742af3c9581370b452d8bd7a4f0f
Diego Freniche Brito <diego@freniche.com>
2014-10-20 11:56:37 +0200

    Created structure for project

--- SwiftCamera/ViewController.swift
+++ SwiftCamera/ViewController.swift
@@ -1,25 +1,12 @@
-//
-//  ViewController.swift
-//  SwiftCamera
-//
-//  Created by Diego Freniche Brito on 20/10/14.
-//  Copyright (c) 2014 Diego Freniche Brito. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
     }
-
 
 }
 
